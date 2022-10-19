106b7c5aad3b84f7501812781c11ed64f265627f
sabuha99 <qemberova1999@mail.ru>
2022-10-19 16:18:51 +0400

    add html_cs/js2

---
+++ js/js2/app.js
@@ -0,0 +1,27 @@
+// task1
+// function quvveteYukselt (n1,n2) {
+//  console.log(n1**n2)
+// }
+// let n1= +prompt("ededi daxil et")
+// let n2= +prompt("ededi daxil et")
+// quvveteYukselt(n1,n2)
+
+// task2
+// function quvveteYukselt (n1,n2) {
+// return(n1**n2)
+// }
+// let show=quvveteYukselt(2,3)
+// alert(show)
+// console.log(quvveteYukselt(2,4))
+// document.write(quvveteYukselt(5,2))
+
+// task3
+// function quvveteYukselt () {
+// let n1= +prompt("ededi daxil et")
+// let n2= +prompt("ededi daxil et")
+// return n1**n2
+// }
+// let show=quvveteYukselt()
+// alert(show)
+// console.log(quvveteYukselt())
+// document.write(quvveteYukselt())
