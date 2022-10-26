b3900c7844e3413094c4c7efc8dccd69a33ff40a
sabuha99 <qemberova1999@mail.ru>
2022-10-26 22:54:09 +0400

    added html_js/js3

---
+++ js/js3/app.js
@@ -0,0 +1,16 @@
+// task1
+    // function add(a,b,callback){
+    // callback (a+b)
+    // }
+
+    // add(3,5,console.log) 
+    // add(3,5,alert)
+
+// task2
+    // function add(callback){
+    // let a=  +prompt()
+    // let b=  +prompt()
+    // callback (a+b)
+    // }
+
+    // add(console.log)
