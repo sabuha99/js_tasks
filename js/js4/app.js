2bef48cc5e518933a7b93ae7a9bfe7f5c30d4669
sabuha99 <qemberova1999@mail.ru>
2022-10-28 23:09:48 +0400

    added html_js/js4

---
+++ js/js4/app.js
@@ -0,0 +1,38 @@
+// task1
+// const calculator={
+// num1:0,
+// num2:0,
+// read: function(n1,n2){
+//   this.num1=+prompt(n1)
+//   this.num2=+prompt(n2)
+// },
+// substract:function(){
+//   console.log(this.num1-this.num2)
+// },
+// sum:function(){
+//   console.log(this.num1+this.num2)
+// },
+// multiply:function(){
+//   console.log(this.num1*this.num2)
+// },
+// divide:function(){
+//  console.log(this.num1/this.num2)
+// }
+// }
+// calculator.read()
+// calculator.substract()
+// calculator.sum()
+// calculator.multiply()
+// calculator.divide()
+
+// task2
+// function User(model, dateofbirth, hostess) {
+//     this.model=model
+//     this.dateofbirth=dateofbirth
+//     this.hostess=hostess
+//     this.doing = ()=> {
+//        console.log(`${this.model} is so nice..`)
+//     }
+//     }
+//     const car = new User('porche',2010,'Sabuha')
+//     car.doing()
