b2e720347d8bd86ae6bcd92b374bdddb3b29c405
sabuha99 <qemberova1999@mail.ru>
2022-11-08 18:19:05 +0400

    added html_js/js6

---
+++ js/js5/app.js
@@ -0,0 +1,38 @@
+// task1
+// let text= "sabuha, artist, amateur"
+// let test=text.split(",")
+// console.log(text)
+
+// task3
+// let text = "Alion Greenheart"
+// let test=text.slice(0,7)
+// console.log(test)
+
+// -----------------
+
+// task2
+// let today=new Date ()
+// document.write(today)
+
+// task3
+// let today=new Date(2022,9,29)
+// let test=today.getDay()
+// document.write(test)
+
+// task4
+// let thismonth=new Date(2022,9)
+// let test=thismonth.getMonth()
+// document.write(test)
+
+// task5
+// let today=Date.now()
+// let age= new Date(+prompt())
+// alert(77-age)
+
+// task6
+// let today=Date.now()
+// let age= new Date(+prompt())
+// console.log((2022-age)) 
+
+
+
---
+++ js/js6/app.js
@@ -0,0 +1,21 @@
+let one=document.getElementById(`div1`)
+one.style.backgroundColor=`black`
+let two=document.getElementById(`div2`)
+two.style.backgroundColor=`red`
+let three=document.getElementById(`div3`)
+three.style.backgroundColor=`yellow`
+let four=document.getElementById(`div4`)
+four.style.backgroundColor=`gray`
+
+one.onclick=()=>{
+document.body.style.backgroundColor=`black`
+}
+two.onclick=()=>{
+document.body.style.backgroundColor=`red`
+    }
+three.onclick=()=>{
+document.body.style.backgroundColor=`yellow`
+    }
+four.onclick=()=>{
+document.body.style.backgroundColor=`gray`
+    }
